2a8d2ba63a09d867c2d8ccb1f78bfa83e80f8eb4
gagare123git config --global user.email lukmanjbadam@gmail.com <lukmanjbadam@gmail.com>
2025-11-20 13:40:34 +0100

    modifed next.config to handle image properlly

--- next.config.js
+++ next.config.js
@@ -1,18 +1,55 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  images: {
+    domains: ["raw.githubusercontent.com"],
+  },
+
   webpack: (config, { isServer }) => {
     if (isServer) {
-      config.externals = [...(config.externals || []), 'styled-jsx', 'styled-jsx/style'];
+      config.externals = [
+        ...(config.externals || []),
+        "styled-jsx",
+        "styled-jsx/style",
+      ];
     }
     return config;
   },
+
   // Don't try to statically generate API routes
   experimental: {
     serverActions: {
-      bodySizeLimit: '2mb',
+      bodySizeLimit: "2mb",
     },
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+
+
+
+
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   webpack: (config, { isServer }) => {
+//     if (isServer) {
+//       config.externals = [...(config.externals || []), 'styled-jsx', 'styled-jsx/style'];
+//     }
+//     return config;
+//   },
+//   // Don't try to statically generate API routes
+//   experimental: {
+//     serverActions: {
+//       bodySizeLimit: '2mb',
+//     },
+//   },
+// }
+
+// module.exports = nextConfig

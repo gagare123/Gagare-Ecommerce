9affca072c54524b4e18e6708afd7ef2796bd919
gagare123git config --global user.email lukmanjbadam@gmail.com <lukmanjbadam@gmail.com>
2025-11-21 23:04:31 +0100

    modified inngest/route.js, config/inngest.js

--- config/db.js
+++ config/db.js
@@ -1,28 +1,56 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose;
+let cached = global.mongoose || { conn: null, promise: null };
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function connectDb() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
+export default async function connectDb() {
+  if (cached.conn) return cached.conn;
   if (!cached.promise) {
-    const opts = {
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, {
       bufferCommands: false,
-    };
-
-    cached.promise = mongoose
-      .connect(process.env.MONGODB_URI, opts)
-      .then((mongoose) => mongoose);
+    });
   }
-
   cached.conn = await cached.promise;
   return cached.conn;
 }
 
-export default connectDb;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+
+// let cached = global.mongoose;
+
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null };
+// }
+
+// async function connectDb() {
+//   if (cached.conn) {
+//     return cached.conn;
+//   }
+
+//   if (!cached.promise) {
+//     const opts = {
+//       bufferCommands: false,
+//     };
+
+//     cached.promise = mongoose
+//       .connect(process.env.MONGODB_URI, opts)
+//       .then((mongoose) => mongoose);
+//   }
+
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
+
+// export default connectDb;
